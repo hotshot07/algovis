be7799a1f8a404fac1cefef2f0b7ba132f82b0dc
hotshot07 <aroram@tcd.ie>
2020-04-24 19:13:24 +0100

    minor changes

--- algovis/__init__.py
+++ algovis/__init__.py
@@ -1,8 +1,10 @@
-__version__ = '0.1.0'
+__version__ = '0.1.0001'
+
 
 def HelloWorld():
     print("Hello :) This is my first package")
     name = 'Mayank'
     print(f"{name}")
+
 
 HelloWorld()
--- tests/test_algovis.py
+++ tests/test_algovis.py
@@ -2,4 +2,4 @@ from algovis import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.0001'
