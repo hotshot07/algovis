29bfb48b9bc548a00cace624cb082e4bbe3e8116
hotshot07 <aroram@tcd.ie>
2020-05-16 20:40:31 +0100

    eval in bubble sort working

--- algovis/sorting/bubblesort.py
+++ algovis/sorting/bubblesort.py
@@ -3,7 +3,6 @@ from ._timer import Timer
 import copy
 
 # TODO
-# info
 # visualize
 
 
@@ -18,7 +17,7 @@ class BubbleSort(BaseClass):
         super().__init__(datalist)
         self._datalist = datalist
 
-    def _ascending_sort(self, iterations):
+    def _ascending_sort(self, steps):
 
         _asc_list = copy.deepcopy(self._datalist)
 
@@ -39,14 +38,14 @@ class BubbleSort(BaseClass):
 
             _number_of_iterations += 1
 
-            if iterations:
+            if steps:
                 print(f'Iteration: {_number_of_iterations}')
                 print(f'List:', *_asc_list)
                 print('\n')
 
         return _asc_list
 
-    def _descending_sort(self, iterations):
+    def _descending_sort(self, steps):
 
         _desc_list = copy.deepcopy(self._datalist)
 
@@ -67,14 +66,15 @@ class BubbleSort(BaseClass):
 
             _number_of_iterations += 1
 
-            if iterations:
+            if steps:
                 print(f'Iteration: {_number_of_iterations}')
                 print(f'List:', * _desc_list)
                 print('\n')
 
         return _desc_list
 
-    def _time_eval(self, reverse, iterations):
+    def _time_eval_asc(self, iterations):
+
         _time_list = copy.deepcopy(self._datalist)
 
         _length_of_list = len(_time_list)
@@ -83,52 +83,91 @@ class BubbleSort(BaseClass):
 
         while iterations:
 
-            timer = Timer()
-            timer.start()
+            _has_swapped = True
 
             _number_of_iterations = 0
-            _has_swapped = True
+
+            timer = Timer()
+            timer.start()
 
             while _has_swapped:
                 _has_swapped = False
 
                 for i in range(_length_of_list - _number_of_iterations - 1):
+                    if _time_list[i] > _time_list[i + 1]:
+                        _time_list[i], _time_list[i + 1] = _time_list[i + 1], _time_list[i]
 
-                    if reverse:
-                        if _time_list[i] < _time_list[i + 1]:
-                            _time_list[i], _time_list[i + 1] = _time_list[i + 1], _time_list[i]
-                            _has_swapped = True
-                    else:
-                        if _time_list[i] > _time_list[i + 1]:
-                            _time_list[i], _time_list[i + 1] = _time_list[i + 1], _time_list[i]
-                            _has_swapped = True
+                        _has_swapped = True
 
-                    _number_of_iterations += 1
+                _number_of_iterations += 1
+
+            stop = timer.stop()
+            _timing_list.append(stop)
+
+            iterations -= 1
+
+            _time_list = copy.deepcopy(self._datalist)
+
+        return _timing_list
+
+    def _time_eval_desc(self, iterations):
+
+        _time_list = copy.deepcopy(self._datalist)
+
+        _length_of_list = len(_time_list)
+
+        _timing_list = []
+
+        while iterations:
+
+            _has_swapped = True
+
+            _number_of_iterations = 0
+
+            timer = Timer()
+            timer.start()
+
+            while _has_swapped:
+                _has_swapped = False
+
+                for i in range(_length_of_list - _number_of_iterations - 1):
+                    if _time_list[i] < _time_list[i + 1]:
+                        _time_list[i], _time_list[i + 1] = _time_list[i + 1], _time_list[i]
+
+                        _has_swapped = True
+
+                _number_of_iterations += 1
 
             stop = timer.stop()
 
             _timing_list.append(stop)
 
             iterations -= 1
+            _time_list = copy.deepcopy(self._datalist)
 
         return _timing_list
 
-    def sort(self, reverse=False, iterations=False):
+    def sort(self, reverse=False, steps=False):
         if reverse:
-            _desc_sorted = BubbleSort._descending_sort(self, iterations)
+            _desc_sorted = BubbleSort._descending_sort(self, steps)
             return _desc_sorted
         else:
-            _asc_sorted = BubbleSort._ascending_sort(self, iterations)
+            _asc_sorted = BubbleSort._ascending_sort(self, steps)
             return _asc_sorted
 
     def evaluate(self, reverse=False, iterations=1):
-        _timing_list = BubbleSort._time_eval(self, reverse, iterations)
+        if reverse:
+            _timing_list = BubbleSort._time_eval_desc(self, iterations)
+        else:
+            _timing_list = BubbleSort._time_eval_asc(self, iterations)
 
-        _timing_list = [str("{:.10f}".format(_elem)) for _elem in _timing_list]
-        # _timing_list = [ for _elem in _timing_list]
+        _minimum_time = str("{:.10f}s".format(min(_timing_list)))
+        _maximum_time = str("{:.10f}s".format(max(_timing_list)))
+        _average_time = str("{:.10f}s".format(sum(_timing_list) / iterations))
 
-        # minimum_time = min(_timing_list)
-        # maximum_time = max(_timing_list)
-        # average_time = sum(_timing_list) / float(iterations)
-
-        return _timing_list
+        eval_dict = {
+            "Minimum Time:": _minimum_time,
+            "Maximum Time:": _maximum_time,
+            "Average Time:": _average_time
+        }
+        return eval_dict
--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from algovis import sorting
+import random
 
-my_list = [1, 1, 1, 1, 1, 1, 1, 1]
+my_list = [random.randint(0, 100) for i in range(10)]
 bs_object = sorting.BubbleSort(my_list)
 #answer = bs_object.sort(iterations=True)
 
-p = bs_object.evaluate(reverse=False, iterations=10)
+p = bs_object.sort(reverse=False, steps=True)
 
 print(p)
