2469e74b7c30b0ae902f44ebf7766e1752498224
hotshot07 <aroram@tcd.ie>
2020-06-30 23:11:44 +0100

    somehow mergesort works?

--- algovis/sorting/__init__.py
+++ algovis/sorting/__init__.py
@@ -1,3 +1,4 @@
 from .bubblesort import BubbleSort
 from .selectionsort import SelectionSort
 from .insertionsort import InsertionSort
+from .mergesort import MergeSort
---
+++ algovis/sorting/mergesort.py
@@ -0,0 +1,109 @@
+"""
+Author: Mayank Arora (hotshot07)
+"""
+
+
+from ._base_class import BaseClass
+from ._timer import Timer
+from ._animation import AnimateAlgorithm
+import copy
+
+
+class MergeSort(BaseClass):
+    def __init__(self, datalist):
+        super().__init__(datalist)
+        self.__datalist = datalist
+
+    def __repr__(self):
+        return f'algovis.sorting.mergesort.MergeSort({self.__datalist})'
+
+    def __ascending_sort_merge_algo(self, _passed_list, start, mid, end):
+        merged = []
+        leftIdx = start
+        rightIdx = mid + 1
+
+        while leftIdx <= mid and rightIdx <= end:
+            if _passed_list[leftIdx] < _passed_list[rightIdx]:
+                merged.append(_passed_list[leftIdx])
+                leftIdx += 1
+            else:
+                merged.append(_passed_list[rightIdx])
+                rightIdx += 1
+
+        while leftIdx <= mid:
+            merged.append(_passed_list[leftIdx])
+            leftIdx += 1
+
+        while rightIdx <= end:
+            merged.append(_passed_list[rightIdx])
+            rightIdx += 1
+
+        for i, sorted_val in enumerate(merged):
+            _passed_list[start + i] = sorted_val
+
+        yield _passed_list
+
+    def _merge_algo(self, _passed_list, start, end):
+        if end <= start:
+            return
+
+        mid = start + ((end - start + 1) // 2) - 1
+        yield from self._merge_algo(_passed_list, start, mid)
+        yield from self._merge_algo(_passed_list, mid + 1, end)
+        yield from self.__ascending_sort_merge_algo(_passed_list, start, mid, end)
+        yield _passed_list
+
+
+    def __asc_animate_sort_it(self):
+        _passed_list = copy.deepcopy(self.__datalist)
+        for i in self._merge_algo(_passed_list, 0, len(_passed_list)-1):
+            yield i
+
+
+
+
+    def visualize(self, reverse=False, interval=250):
+        _vis_list = copy.deepcopy(self.__datalist)
+
+        if not reverse:
+            AnimateAlgorithm("Merge Sort", _vis_list, self.__asc_animate_sort_it(), interval)
+        else:
+            AnimateAlgorithm("Merge Sort", _vis_list, self.__descending_sort_algo(), interval)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# _iteration_dict = {}
+# iterations = 0
+#
+# if not reverse:
+#     for _yielded_list in self.__ascending_sort_algo():
+#         iterations += 1
+#         _iteration_dict[iterations] = copy.deepcopy(_yielded_list)
+# else:
+#     for _yielded_list in self.__descending_sort_algo():
+#         iterations += 1
+#         _iteration_dict[iterations] = copy.deepcopy(_yielded_list)
+
+# if steps:
+#     print()
+#     print("Iteration    List")
+#     for _iter, _list in _iteration_dict.items():
+#         print("    " + str(_iter) + "        " + str(_list))
+#
+#     print()
+# return _iteration_dict
