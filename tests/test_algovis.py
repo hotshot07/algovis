64ad8f2eb59f745f8e5fc4c29bca0ca4eb50a579
hotshot07 <aroram@tcd.ie>
2020-07-24 03:40:34 +0100

    added tests

--- tests/test_algovis.py
+++ tests/test_algovis.py
@@ -1,12 +1,20 @@
 from algovis import __version__
+
 from algovis import sorting
+from algovis import searching
 
 
 def test_version():
-    assert __version__ == '0.1.2'
+    assert __version__ == '0.1.4'
 
 
 def test_sorting():
     mylist = [4, 3, 2, 1]
     A = sorting.BubbleSort(mylist)
     assert A.sort() == [1, 2, 3, 4]
+
+
+def test_searching():
+    mylist = [1, 2, 3, 4, 5]
+    bs = searching.BinarySearch(mylist)
+    assert bs.search(3) == None
